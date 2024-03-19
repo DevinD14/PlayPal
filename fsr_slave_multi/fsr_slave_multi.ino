918b63c659c1e135a3337495f444a62c23abf109
Devin Davis <devintdavis14@gmail.com>
2024-03-19 16:37:52 -0400

    Updated Multi Input code for HC-05 Shield implementation

--- fsr_slave_multi/fsr_slave_multi.ino
+++ fsr_slave_multi/fsr_slave_multi.ino
@@ -2,39 +2,37 @@
 #define led 4
 #define led2 5
 
-// Define variables:
-int val; // The reading from the serial
-
 void setup() {
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
-  // Set led as output:
+  // Set led pins as output:
   pinMode(led, OUTPUT);
   pinMode(led2, OUTPUT);
 }
 
 void loop() {
-  // Checks if there is an input available in the Serial:
-  if(Serial.available())
-  {
-    // Read the FSR pin and store the output as val:
-    val = Serial.read();
-    // Checks if the digital pin is a 0 or 1 to turn the appropriate LED off or on respectively:
-    if(val == 97)
-    {
-      digitalWrite(led, LOW);
-    }
-    else if(val == 65)
-    {
-      digitalWrite(led, HIGH);
-    }
-    if(val == 98)
-    {
-      digitalWrite(led2, LOW);
-    }
-    else if(val == 66)
-    {
-      digitalWrite(led2, HIGH);
+  // Check if there is serial data available to read:
+  if (Serial.available() > 0) {
+    // Read the incoming byte:
+    int val = Serial.read();
+
+    // Debugging: Print the received value to the serial monitor
+    Serial.println(val);
+
+    // Determine the action based on the received character:
+    switch (val) {
+      case 97: // ASCII 97
+        digitalWrite(led, LOW);
+        break;
+      case 65: // ASCII 65
+        digitalWrite(led, HIGH);
+        break;
+      case 98: // ASCII 98
+        digitalWrite(led2, LOW);
+        break;
+      case 66: // ASCII 66
+        digitalWrite(led2, HIGH);
+        break;
     }
   }
 }
